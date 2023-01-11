5baa1a7a90c4fe45d421b9c9104368a12e015456
JacquelineTuyisenge <j.tuyisenge@alustudent.com>
2023-01-11 14:01:28 -0800

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,19 +10,19 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *m;
+	char *cr;
 	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
 
-	m = malloc(sizeof(c) * size);
+	cr = malloc(sizeof(c) * size);
 
-	if (m == NULL)
+	if (cr == NULL)
 		return (NULL);
 
-	for (i= 0; i < size; i++)
-		m[i] = c;
+	for (i = 0; i < size; i++)
+		cr[i] = c;
 
-	return (m);
+	return (cr);
 }
