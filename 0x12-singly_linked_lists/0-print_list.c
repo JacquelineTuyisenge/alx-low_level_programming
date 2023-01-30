0b1146fe7490c155d9e3ca35edd63003db298c55
JacquelineTuyisenge <j.tuyisenge@alustudent.com>
2023-01-30 15:32:46 -0800

    linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t m = 0;
+	size_t s = 0;
 
 	while (h)
 	{
 		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %m\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		m++;
+		s++;
 	}
 
-	return (m);
+	return (s);
 }
